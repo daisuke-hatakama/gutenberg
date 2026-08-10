6341f99665b779c1c62bf2f20b43ced95a9292f1
Jorge Costa <jorge.costa@developer.pt>
2026-08-10 17:07:11 +0100

    Block Editor: extract getPopulatedCategories util and expose pattern explorer sidebar via private APIs

--- packages/block-editor/src/private-apis.js
+++ packages/block-editor/src/private-apis.js
@@ -9,7 +9,10 @@ import {
 	extractWords,
 	getNormalizedSearchTerms,
 	normalizeString,
+	searchItems,
 } from './components/inserter/search-items';
+import PatternExplorerSidebar from './components/inserter/block-patterns-explorer/pattern-explorer-sidebar';
+import { getPopulatedCategories } from './components/inserter/block-patterns-tab/utils';
 import { PrivateListView } from './components/list-view';
 import InspectorControlsLastItem from './components/inspector-controls/last-item';
 import { useHasBlockToolbar } from './components/block-toolbar/use-has-block-toolbar';
@@ -159,4 +162,7 @@ lock( privateApis, {
 	InnerContent,
 	useNativeUndo,
 	usesNativeUndo,
+	PatternExplorerSidebar,
+	getPopulatedCategories,
+	searchItems,
 } );
