bc9dcd28d66353a9182e07f194fb4ca7c78d1f33
Andrew Duthie <andrew.duthie@automattic.com>
2026-08-13 15:20:02 -0400

    Revert "TEMPORARY: Un-exempt Story to demonstrate failure"
    This reverts commit 3778431390538cb58f6dcbac0dfc2780d48b4c98.

--- packages/components/src/animate/stories/index.story.tsx
+++ packages/components/src/animate/stories/index.story.tsx
@@ -89,8 +89,7 @@ Loading.args = {
 Loading.parameters = {
 	// FIXME: Loading demo applies the opacity pulse to a Notice (and its text), which fails color-contrast mid-animation. Update the story to animate a non-text element instead of Notice.
 	// See: https://github.com/WordPress/gutenberg/issues/81596
-	// TEMPORARY: Commented to demonstrate failure.
-	// a11y: { test: 'todo' },
+	a11y: { test: 'todo' },
 };
 
 export const SlideIn = Template.bind( {} );
